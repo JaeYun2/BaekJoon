f273dd92880438af5b29500bcd569366a44a0c1b
JaeYun2 <goldkevin01@naver.com>
2023-06-16 14:16:30 +0900

    quick_sort, merge_sort

--- 1_silver/boj_2751_1.c
+++ 1_silver/boj_2751_1.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 void quick_sort(int length, int start, int *arr);
+void merge_sort(int start, int end, int* arr,int* cal);
 
 int main(int argc, char **argv)
 {
@@ -14,7 +15,15 @@ int main(int argc, char **argv)
         scanf("%d",arr+i);
     }
 
-    quick_sort(0,N-1,arr);
+    //quick_sort(0,N-1,arr); for quick sort
+    
+
+    ///* for Merge sort   
+    int *cal=(int*)malloc(sizeof(int)*N);
+    merge_sort(0,N, arr, cal);
+    free(cal);
+    //*/
+
     for(int i=0;i<N;i++) printf("%d\n",arr[i]);
     free(arr);
     return 0;
@@ -37,5 +46,50 @@ void quick_sort(int start, int end,int *arr)
 
         quick_sort(start,i-1,arr);
         quick_sort(j+1,end,arr);
+    }
+}
+
+void merge_sort(int start, int end, int* arr, int* cal)
+{
+    if(end-start>1)
+    {
+        int t=(end-start)/2,p=start+t;
+        merge_sort(start,p,arr,cal);
+        merge_sort(p,end,arr,cal);
+
+
+        int i=start, cnt=start, j=p;
+        while(i<p&&j<end)
+        {
+            if(arr[i]<arr[j])
+            {
+                cal[cnt]=arr[i];
+                i++;
+            }
+            else
+            {
+                cal[cnt]=arr[j];
+                j++;
+            }
+            cnt++;
+        }
+        
+        if(i<p) 
+        {
+            for(int k=i;k<p;k++)
+            {
+                cal[cnt]=arr[k];
+                cnt++;
+            }
+        }
+        else{
+            for(int k=j;k<end;k++)
+            {
+                cal[cnt]=arr[k];
+                cnt++;
+            }
+        }
+
+        for(int k=start;k<end;k++) arr[k]=cal[k];
     }
 }
